18a99609e5713f4c55b0420f852a7a712440d0e0
DESKTOP-URCM5RQ\JACZAW <zawislak.jacek@gmail.com>
2018-07-19 22:15:51 +0200

    Nowy projekt

---
+++ src/main/java/pl/kz/championshipsfootballapp/ChampionshipsFootballAppApplication.java
@@ -0,0 +1,12 @@
+package pl.kz.championshipsfootballapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionshipsFootballAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampionshipsFootballAppApplication.class, args);
+	}
+}
